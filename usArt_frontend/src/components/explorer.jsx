5a6041331aa81ede2eff63bbff688e011a183702
rogeranyor <ranorgra18@alumnes.ub.edu>
2022-10-20 21:19:50 +0200

    css changes in explorer && navbar title ( z-index)

--- usArt_frontend/src/components/explorer.jsx
+++ usArt_frontend/src/components/explorer.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import './explorerStyles.css'
-
+import imageP from  '../assets/image1.jpg'
+import imageP2 from  '../assets/pincel.jpg'
 function renderCard(card, index) {
   fetch("https://api.example.com/items")
       .then(res => res.json())
@@ -22,12 +23,11 @@ function renderCard(card, index) {
         }
       )
   return (
-    <div class="card box border-0" key={index}>
-      <img src={card.image} class="card-img-top size" alt="imagen_pincel" ></img>
+    <div class="card custom" key={index}>
+      <img src={card.image} class="card-img-top size-img" alt="imagen_pincel" ></img>
       <div class="card-body">
         <h5 style={{color:"black"}}class="card-title">{card.title}</h5>
         <p class="card-text max">{card.descripcion}</p>
-        <a href="#/" class="btn btn-primary">Contactar</a>
       </div>
     </div>)
 }
@@ -36,21 +36,21 @@ export default class explorer extends Component {
   constructor(props) {
     super(props);
     this.cards = [
-      { image: "https://ik.imagekit.io/ikmedia/women-dress-2.jpg", title: "card 1", descripcion: "hola soy la card 1hola soy la card 1hola soy la cardaaaa 1hola soy laaaa card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1hola soy la card 1" },
-      { image: "https://ik.imagekit.io/ikmedia/women-dress-2.jpg", title: "card 2", descripcion: "hola soy la card 2" },
-      { image: "https://ik.imagekit.io/ikmedia/women-dress-2.jpg", title: "card 3", descripcion: "hola soy la card 3" },
-      { image: "https://ik.imagekit.io/ikmedia/women-dress-2.jpg", title: "card 4", descripcion: "hola soy la card 4" },
-      { image: "https://ik.imagekit.io/ikmedia/women-dress-2.jpg", title: "card 5", descripcion: "hola soy la card 5" },]
+      { image: imageP, title: "card 1", descripcion: "hola soy la card 1 y vendo vestidos de boda hechos a mano con el mejor material hola soy la card 1 y vendo vestidos de boda hechos a mano con el mejor material" },
+      { image: imageP2, title: "card 2", descripcion: "hola soy la card 1 y vendo vestidos de boda hechos a mano con el mejor material hola soy la card 1 y vendo vestidos de boda hechos a mano con el mejor material" },
+      { image: imageP, title: "card 3", descripcion: "hola soy la card 3" },
+      { image: imageP2, title: "card 4", descripcion: "hola soy la card 4" },
+      { image: imageP, title: "card 5", descripcion: "hola soy la card 5" },]
 
   }
   render() {
     return (
       <div>
-        <div class="row header">
+        <div class="row header border">
           <h1>Explore el talento en UsArt</h1>
           <p style={{color:"white"}}>Miles de personas ofrecen servicios de arte diariamente</p>
         </div>
-        <div class="grid">
+        <div class="grid ">
           {this.cards.map(renderCard)}
         </div>
       </div>)
