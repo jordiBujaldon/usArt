4c0263a269bd9bf42837b8a17a71cb07505e20ae
guillesmar <guillesmar3@gmail.com>
2022-10-20 20:55:39 +0200

    Conexion base de datos

--- usArt_backend/usArt_backend/settings.py
+++ usArt_backend/usArt_backend/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'usArt_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'usart-db',
+        'USER': 'server_db_admin@usart-db-server',
+        'PASSWORD': '!usart_ES_A3_password!',
+        'HOST': 'usart-db-server.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
+    
     }
 }
 
