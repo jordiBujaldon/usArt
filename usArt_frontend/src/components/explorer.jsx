996bc5c646a29f6c75acf46af81e00fbee6773e3
rogeranyor <ranorgra18@alumnes.ub.edu>
2022-10-25 17:01:57 +0200

    Now working

--- usArt_frontend/src/components/explorer.jsx
+++ usArt_frontend/src/components/explorer.jsx
@@ -5,32 +5,11 @@ import imageP2 from '../assets/pincel.jpg'
 import { useState, useEffect } from "react";
 
 
-const errorHander = (error) =>{
-  const img = document.getElementById(error)
-  img.src = imageP
-  img.style.objectFit="cover"
+export default function Explorer() {
 
-  
-}
+  const [cards, setCards] = useState([]);
 
-function RenderCard(card, index) {
-  return (
-    <div class="card custom" key={index}>
-      <img id={index} src={card.image} onerror={errorHander(index)}
-        class="card-img-top size-img" alt="Sorry! not available at this time" ></img>
-      <div class="card-body">
-        <h5 style={{ color: "black" }} class="card-title">{card.title}</h5>
-        <p >{card.price}€</p>
-        <p class="card-text max">{card.description}</p>
-      </div>
-    </div>)
-}
-
-export default function Explorer(){
-  
-  const [cards,setCards] = useState([]);
-
-  useEffect(callApi,[])
+  useEffect(callApi, [])
 
   function callApi() {
     fetch(
@@ -44,20 +23,32 @@ export default function Explorer(){
 
   if (cards.length === 0) return <div class="errorApi"><h1>Cargando..</h1></div>
 
-  
- 
-
+  function RenderCard(card, index) {
     return (
-      <div>
-        <div class="row header border">
-          <h1>Explore el talento en UsArt</h1>
-          <p style={{ color: "white" }}>Miles de personas ofrecen servicios de arte diariamente</p>
-        </div>
-        <div class="grid ">
-          {cards.map(RenderCard)}
+      <div class="card custom" key={index}  >
+        <img id={index} src={card.image} 
+          class="card-img-top size-img" alt="Sorry! not available at this time" ></img>
+        <div class="card-body">
+          <h5 style={{ color: "black" }} class="card-title">{card.title}</h5>
+          <p >{card.price}€</p>
+          <p class="card-text max">{card.description}</p>
         </div>
       </div>)
   }
-  
+ 
+
+
+  return (
+    <div>
+      <div class="row header border">
+        <h1>Explore el talento en UsArt</h1>
+        <p style={{ color: "white" }}>Miles de personas ofrecen servicios de arte diariamente</p>
+      </div>
+      <div class="grid ">
+        {cards.map(RenderCard)}
+      </div>
+    </div>)
+}
+
 
 
